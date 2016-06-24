d2d8250df22d5a5196918ef658961a507f95253c
MrPaulPhan <paul@mrpaulphan.com>
2016-06-24 18:43:32 -0400

    first card styling

--- source/js/03-modules/settings.js
+++ source/js/03-modules/settings.js
@@ -3,6 +3,8 @@ function settings() {
         weight: "lbs",
         formula: "brzycki"
     }
+
+    var weightValue = $('#weightValue');
     var currentWeight = localStorage.getItem("weight");
     var currentFormula = localStorage.getItem("formula");
     if (currentWeight == null) {
@@ -11,5 +13,17 @@ function settings() {
     if (currentFormula == null) {
         localStorage.setItem("formula", defaultSettings.formula);
     }
-    console.log("Current Settings: Weight:" +currentWeight+ " Formula:" +currentFormula);
+    console.log("Current Settings: Weight:" + currentWeight + " Formula:" + currentFormula);
+
+    // Assign weight format
+    weightValue.attr('data-weight', currentWeight);
+    weightValue.attr('placeholder', '135' + currentWeight);
+    weightValue.keypress(function(e) {
+        //if the letter is not digit then display error and don't type anything
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            //display error message
+            return false;
+        }
+    });
+
 }
